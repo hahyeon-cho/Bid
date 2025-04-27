fb5a23209c84b7e41304ab74fdd04468df01800e
hahyeon-cho <mondlicht3724@gmail.com>
2025-04-27 17:41:59 +0900

    chore: 경매 내역 조회 관련 import 누락분 추가

--- src/main/java/com/kcs3/bid/repository/AuctionInfoRepository.java
+++ src/main/java/com/kcs3/bid/repository/AuctionInfoRepository.java
@@ -1,6 +1,6 @@
 package com.kcs3.bid.repository;
 
-import com.kcs3.bid.dto.AuctionInfoSummeryDto;
+import com.kcs3.bid.dto.AuctionInfoDto;
 import com.kcs3.bid.entity.AuctionInfo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
